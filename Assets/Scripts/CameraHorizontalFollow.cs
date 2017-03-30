0a002a9261575bf66e60408102768db26ca5632b
sarimoly <sarimoly@163.com>
2017-03-30 18:58:10 +0800

    camera follow

--- Assets/Scripts/CameraHorizontalFollow.cs
+++ Assets/Scripts/CameraHorizontalFollow.cs
@@ -6,7 +6,12 @@ public class CameraHorizontalFollow : MonoBehaviour {
     public float xAngle = 15f;
     public string targetTag = "Player";
     public Transform camTransform;
-    public Transform target;
+    public GameObject target;
+    public AvatarController avatar;
+    public float xSmooth = 5;
+    public bool stop = false;
+
+    public Vector3 moveToPos;
 
     void Awake()
     {
@@ -21,17 +26,13 @@ public class CameraHorizontalFollow : MonoBehaviour {
 
     Vector3 calcutePos()
     {
-        if (target != null)
-        {
-            var pos = target.position + new Vector3(0, zDis * Mathf.Tan(xAngle * Mathf.PI / 180), -zDis);
-            return pos;
-        }
-        return Vector3.zero;
+        var pos = target.transform.position + new Vector3(0, zDis * Mathf.Tan(xAngle * Mathf.PI / 180), -zDis);
+        return pos;
     }
 
     void FindTarget()
     {
-        target = GameObject.FindGameObjectWithTag(targetTag).transform;
+        target = GameObject.FindGameObjectWithTag(targetTag);
         if (target == null)
         {
             Debug.LogWarning("Can not find camera follow target tag = " + targetTag);
@@ -41,6 +42,7 @@ public class CameraHorizontalFollow : MonoBehaviour {
         {
             Debug.Log("Find look target ", target);
         }
+        avatar = target.GetComponent<AvatarController>();
         return;
     }
 
@@ -48,10 +50,47 @@ public class CameraHorizontalFollow : MonoBehaviour {
     {
         camTransform.position = calcutePos();
         camTransform.eulerAngles = new Vector3(xAngle, 0, 0);
+
+        moveToPos = camTransform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            if (avatar.moving == false && moveToPos != camTransform.position)
+            {
+                //Vector2 targetP = Camera.main.WorldToScreenPoint(target.transform.position);
+                if (avatar.m_Direction ==  1)
+                {
+                    moveToPos = calcutePos() + new Vector3(0, 1, 0) * 1;
+                }
+                else if (avatar.m_Direction == -1)
+                {
+                    moveToPos = calcutePos() - new Vector3(0, 1, 0) * 1;
+                }
+
+                var targetX = Mathf.Lerp(camTransform.position.x, moveToPos.x, xSmooth * Time.deltaTime);
+                targetX = Mathf.Clamp(targetX, camTransform.position.x, moveToPos.x);
+                //var targetY = Mathf.Lerp(camTransform.position.x, moveToPos.y, xSmooth * Time.deltaTime);
+                //var targetZ = Mathf.Lerp(camTransform.position.x, moveToPos.z, xSmooth * Time.deltaTime);
+                //targetZ = Mathf.Clamp(targetZ, camTransform.position.z, moveToPos.z);
+
+                camTransform.position = new Vector3(targetX, camTransform.position.y, camTransform.position.z);
+                if (Mathf.Abs(targetX - moveToPos.x) < 0.05f)
+                    camTransform.position = moveToPos;
+            }
+            if (avatar.moving)
+            {
+                Vector2 targetP = Camera.main.WorldToScreenPoint(target.transform.position);
+                if (targetP.x >= Screen.width * 0.5)
+                    camTransform.position = new Vector3(Screen.width * 0.5f, target.transform.position.y, target.transform.position.z);
+            }
+        }
+    }
 }
